3a50ef139b37f3f3c7f4b059cb5e4b1b13559ead
cpplovelove <gsgslolo@naver.com>
2023-09-18 22:38:28 +0900

    test

--- containers/WebRTC/WebRTCContainer.tsx
+++ containers/WebRTC/WebRTCContainer.tsx
@@ -31,7 +31,7 @@ import RTCVideo from './RTCVideo';
 import socketPromise from './socketPromise';
 
 // const MEDIA_SERVER_URL = 'http://localhost:8080';
-const MEDIA_SERVER_URL = 'https://learniverse-media.kro.kr:8080';
+const MEDIA_SERVER_URL = 'http://3.128.247.52:8080';
 
 const WebRTCContainer = () => {
   const router = useRouter();
